dbf0f6f371ea4a8ef33bd76d4be9e2a1593e31d6
hmisonne <helene.misonne@gmail.com>
2020-07-02 08:02:01 -0700

    Refactor: Create a separate component for CardBoard

--- App.js
+++ App.js
@@ -1,14 +1,9 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
-import Constants from 'expo-constants';
-import { FlatGrid } from 'react-native-super-grid';
-import WordCard from './components/WordCard'
-import WordInput from './components/WordInput'
-import {cardList, playerInfo, roundInfo} from './data'
-import TeamScore from './components/TeamScore'
+import ClueForm from './components/ClueForm'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import CardBoard from './components/CardBoard'
 
 const Stack = createStackNavigator()
 
@@ -20,6 +15,7 @@ export default class App extends React.Component {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Word Guessing Game" component={CardBoard} />
+          <Stack.Screen name="Submit a Clue" component={ClueForm} />
         </Stack.Navigator>
       </NavigationContainer>
 
@@ -27,102 +23,4 @@ export default class App extends React.Component {
   }
   
 }
-
-class CardBoard extends React.Component {
-  state = {
-    wordSelection: cardList,
-    roundInfo
-  }
-  onSelectWord = (cardId) => {
-    const {wordSelection} = this.state
-
-    wordSelection.map(word => {
-      word.cardId !== cardId 
-      ? word
-      : word.cardGuessed = true
-    })
-    this.setState((prevState)=>({
-      wordSelection
-    }))
-  }
-
-  onSubmitClue = (clueGiven) => {
-    this.setState((prevState) => ({
-      roundInfo: {
-        ...prevState.roundInfo,
-        clueGiven
-      } 
-    }))
-  }
-  render(){
-    const {wordSelection, roundInfo} = this.state
-    const orangeCards = wordSelection.filter(card => card.cardColorId === 'orange')
-    const purpleCards = wordSelection.filter(card => card.cardColorId === 'purple')
-    return (
-    <View style={styles.container}>
-      <Text>
-        Hello {playerInfo.playerName}!
-      </Text>
-      {playerInfo.clueMaster &&
-        <WordInput
-          playerInfo= {playerInfo}
-          roundInfo= {roundInfo}
-          onSubmit = {this.onSubmitClue}
-        />
-      }
-      <Text>Clue: {roundInfo.clueGiven}</Text>
-        <TeamScore
-          cards = {orangeCards}
-          color='orange'
-        />
-  
-        <TeamScore
-          cards = {purpleCards}
-          color='purple'
-        /> 
-        <FlatGrid
-          itemDimension={100}
-          data={wordSelection}
-          style={styles.gridView}
-          spacing={10}
-          renderItem={({ item }) => (
-            <WordCard
-              key={item.cardId} 
-              onSelectWord={() => this.onSelectWord(item.cardId)}
-              word={item}
-              clueMaster= {playerInfo.clueMaster}/>
-            )}
-        />
-      </View>
-        
-    )
-  }
-  
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  gridView: {
-    marginTop: 10,
-    flex: 1,
-  },
-  
-  button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
 
---
+++ components/CardBoard.js
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
+import { FlatGrid } from 'react-native-super-grid';
+import WordCard from './WordCard'
+import WordInput from './WordInput'
+import {cardList, playerInfo, roundInfo} from '../data'
+import TeamScore from './TeamScore'
+import ClueForm from './ClueForm'
+
+export default class CardBoard extends React.Component {
+    state = {
+      wordSelection: cardList,
+      roundInfo
+    }
+    onSelectWord = (cardId) => {
+      const {wordSelection} = this.state
+  
+      wordSelection.map(word => {
+        word.cardId !== cardId 
+        ? word
+        : word.cardGuessed = true
+      })
+      this.setState((prevState)=>({
+        wordSelection
+      }))
+    }
+  
+    onSubmitClue = (clueGiven) => {
+      this.setState((prevState) => ({
+        roundInfo: {
+          ...prevState.roundInfo,
+          clueGiven
+        } 
+      }))
+    }
+    render(){
+      const {wordSelection, roundInfo} = this.state
+      const orangeCards = wordSelection.filter(card => card.cardColorId === 'orange')
+      const purpleCards = wordSelection.filter(card => card.cardColorId === 'purple')
+      return (
+      <View style={styles.container}>
+        <Text>
+          Hello {playerInfo.playerName}!
+        </Text>
+        {playerInfo.clueMaster &&
+          <WordInput
+            playerInfo= {playerInfo}
+            roundInfo= {roundInfo}
+            onSubmit = {this.onSubmitClue}
+          />
+        }
+        <Text>Clue: {roundInfo.clueGiven}</Text>
+          <TeamScore
+            cards = {orangeCards}
+            color='orange'
+          />
+    
+          <TeamScore
+            cards = {purpleCards}
+            color='purple'
+          /> 
+          <FlatGrid
+            itemDimension={100}
+            data={wordSelection}
+            style={styles.gridView}
+            spacing={10}
+            renderItem={({ item }) => (
+              <WordCard
+                key={item.cardId} 
+                onSelectWord={() => this.onSelectWord(item.cardId)}
+                word={item}
+                clueMaster= {playerInfo.clueMaster}/>
+              )}
+          />
+        </View>
+          
+      )
+    }
+    
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      paddingTop: Constants.statusBarHeight,
+      backgroundColor: '#ecf0f1',
+      padding: 8,
+    },
+    gridView: {
+      marginTop: 10,
+      flex: 1,
+    },
+    
+    button: {
+      alignItems: "center",
+      backgroundColor: "#DDDDDD",
+      padding: 10
+    },
+    paragraph: {
+      margin: 24,
+      fontSize: 18,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+  });
+  
